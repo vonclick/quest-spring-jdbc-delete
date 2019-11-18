97ecc79cb930395815835ec4b1d9918187324ea7
Arsene Ella <aellalongin@gmai.com>
2019-11-18 17:48:16 +0100

    quest spring jdbc4 deletete DONE

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -13,9 +13,33 @@ public class SchoolRepository {
     private final static String DB_PASSWORD = "Horcrux4life!";
 
     public void deleteById(Long id) {
+    	
+        // TODO: delete a school from the database    	
+        try {
+            Connection connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD
+            );
+            PreparedStatement statement = connection.prepareStatement(
+                    "DELETE FROM school WHERE id=?"
+            );
+            statement.setLong(1, id);
+
+            if (statement.executeUpdate() != 1) {
+                throw new SQLException("failed to delete data");
+            }
+        } 
+        catch (SQLException e) {
+            e.printStackTrace();
+        }    	
         // TODO: delete a school from the database
+    	
     }
 
+    
+    
+    
+    
+    
     public List<School> findAll() {
 
         try {
